e66eacce8ed345556760860e415e46cfd7c31412
Habtamu <habtamuadugna.d@gmail.com>
2023-06-16 09:21:53 +0300

    transactions

--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 public interface StudentRepository extends JpaRepository<Student, Long>{
 
     @Query("SELECT s FROM Student s WHERE s.email=?1")
